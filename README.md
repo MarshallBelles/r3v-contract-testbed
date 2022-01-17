fe33490708b2c25a32fe251e8696bb2fead9f1d3
Marshall Belles <marshall.belles@milwaukeetool.com>
2022-01-17 13:27:33 -0500

    updating flow-ts version, fixing test issues

---
+++ README.md
@@ -0,0 +1,16 @@
+# R3V-CONTRACT-TESTBED
+
+Before running tests, ensure you have the Flow CLI installed.
+
+Then CD into the directory and execute `npm i`.
+
+To execute the tests, execute `npm run test`.
+
+
+# DOCKER
+
+If you are *not* running on MacOS, Docker may be preferable.
+
+* CD into the directory
+* `docker build . -t r3v-testing`
+* `docker run r3v-testing`
--- tests/unit-tests/contract.spec.ts
+++ tests/unit-tests/contract.spec.ts
@@ -1,49 +1,54 @@
 import 'jest';
-import { Flow, FlowKey, FlowNetwork, Proposal, TransactionResultResponse } from '../../flow-ts';
+import { Flow, FlowNetwork, AccountKey, TransactionResultResponse } from '@marshallbelles/flow-ts';
 import * as fs from 'fs';
 import { exec, ChildProcess } from 'child_process';
-import { gzip } from 'zlib';
+import { gzip, gunzip } from 'zlib';
 
 describe('ContractTesting', () => {
     let flow: Flow;
-    let svc: Proposal;
-    let usr1: Proposal;
-    let usr2: Proposal;
+    let svc: AccountKey;
+    let usr1: AccountKey;
+    let usr2: AccountKey;
     let emulator: ChildProcess;
 
     beforeAll(async () => {
         // start emulator
-        emulator = exec('flow emulator -f flow.json');
+        emulator = exec('flow emulator');
         // wait 1 second
         await new Promise<void>((p) => setTimeout(p, 1000));
-        const key0: FlowKey = {
-            keyID: 0,
-            private: 'ec8cd232a763fb481711a0f9ce7d1241c7bc3865689afb31e6b213d781642ea7',
-            public: '81c12390330fdbb55340911b812b50ce7795eefe5478bc5659429f41bdf83d8b6b50f9acc730b9cae67dc29e594ade93cac33f085f07275b8d45331a754497dd',
-        };
         svc = {
-            address: Buffer.from('f8d6e0586b0a20c7', 'hex'),
-            privateKey: 'ec8cd232a763fb481711a0f9ce7d1241c7bc3865689afb31e6b213d781642ea7',
-            publicKey: '81c12390330fdbb55340911b812b50ce7795eefe5478bc5659429f41bdf83d8b6b50f9acc730b9cae67dc29e594ade93cac33f085f07275b8d45331a754497dd',
-        }
+            id: 0,
+            address: 'f8d6e0586b0a20c7',
+            private_key: Buffer.from('ec8cd232a763fb481711a0f9ce7d1241c7bc3865689afb31e6b213d781642ea7', 'hex'),
+            public_key: Buffer.from('81c12390330fdbb55340911b812b50ce7795eefe5478bc5659429f41bdf83d8b6b50f9acc730b9cae67dc29e594ade93cac33f085f07275b8d45331a754497dd', 'hex'),
+            hash_algo: 3,
+            sign_algo: 2,
+            weight: 1000,
+        };
         // connect to emulator
-        flow = new Flow(FlowNetwork.EMULATOR, '0xf8d6e0586b0a20c7', [key0], 5);
+        flow = new Flow(FlowNetwork.EMULATOR, '0xf8d6e0586b0a20c7', [svc], 5);
         await flow.start();
         // create usr1 and usr2 accounts for testing
-        let acct1 = await flow.create_account(['54cfa0f49e1364255eb5ac6b3b5a6fd5a23cf9a786c39640a5a0ccd9d257c85d1de75d0f928ad504af4a9791e9d1b9ed4faae0149b0ffb75094cbea4c23fc1f1']);
-        if (acct1 instanceof Error) return Promise.reject(acct1);
         usr1 = {
-            address: Buffer.from(acct1.events.filter((x) => x.type == 'flow.AccountCreated')[0].payload.value.fields[0].value.value.replace(/\b0x/g, ''), 'hex'),
-            privateKey: 'ac4fdb02a932bc4a0cae0987258316727ede1973f784b91260f5bfeccfebb900',
-            publicKey: '54cfa0f49e1364255eb5ac6b3b5a6fd5a23cf9a786c39640a5a0ccd9d257c85d1de75d0f928ad504af4a9791e9d1b9ed4faae0149b0ffb75094cbea4c23fc1f1'
-        }
-        let acct2 = await flow.create_account(['fd8e43b88a5042e9dce0c5d0d0014455ec5dd512efed92a104381e21592baf23c171a82a7b5c2714d4e05af12a2c95ba052ab1b54c265451f3ae8b0cec370c2b'])
-        if (acct2 instanceof Error) return Promise.reject(acct2);
+            private_key: Buffer.from('ac4fdb02a932bc4a0cae0987258316727ede1973f784b91260f5bfeccfebb900', 'hex'),
+            public_key: Buffer.from('54cfa0f49e1364255eb5ac6b3b5a6fd5a23cf9a786c39640a5a0ccd9d257c85d1de75d0f928ad504af4a9791e9d1b9ed4faae0149b0ffb75094cbea4c23fc1f1', 'hex'),
+            hash_algo: 3,
+            sign_algo: 2,
+            weight: 1000,
+        };
+        const acct1 = await flow.create_account([usr1]);
+        if (acct1 instanceof Error) return Promise.reject(acct1);
+        usr1.address = acct1.events.filter((x) => x.type == 'flow.AccountCreated')[0].payload.value.fields[0].value.value.replace(/\b0x/g, '');
         usr2 = {
-            address: Buffer.from(acct2.events.filter((x) => x.type == 'flow.AccountCreated')[0].payload.value.fields[0].value.value.replace(/\b0x/g, ''), 'hex'),
-            privateKey: '09aa0e3bff02c0a2e9d0e495b5628c0f295976e11af2900d83d87426f1a3839b',
-            publicKey: 'fd8e43b88a5042e9dce0c5d0d0014455ec5dd512efed92a104381e21592baf23c171a82a7b5c2714d4e05af12a2c95ba052ab1b54c265451f3ae8b0cec370c2b'
-        }
+            private_key: Buffer.from('ac4fdb02a932bc4a0cae0987258316727ede1973f784b91260f5bfeccfebb900', 'hex'),
+            public_key: Buffer.from('54cfa0f49e1364255eb5ac6b3b5a6fd5a23cf9a786c39640a5a0ccd9d257c85d1de75d0f928ad504af4a9791e9d1b9ed4faae0149b0ffb75094cbea4c23fc1f1', 'hex'),
+            hash_algo: 3,
+            sign_algo: 2,
+            weight: 1000,
+        };
+        const acct2 = await flow.create_account([usr1]);
+        if (acct2 instanceof Error) return Promise.reject(acct2);
+        usr2.address = acct2.events.filter((x) => x.type == 'flow.AccountCreated')[0].payload.value.fields[0].value.value.replace(/\b0x/g, '');
     });
 
     afterAll(async () => {
@@ -64,7 +69,7 @@ describe('ContractTesting', () => {
         expect(flow).toBeInstanceOf(Flow);
         const transaction: string = fs.readFileSync('cadence/T_MINT_NFTS.cdc').toString('utf-8')
             // service account
-            .replace(/[$](service)/g, svc.address.toString('hex'));
+            .replace(/[$](service)/g, svc.address);
 
         const metadataForNFT1 = {
             ID: 1,                                                      // this is our R3V ID, not the NFT id
@@ -100,11 +105,11 @@ describe('ContractTesting', () => {
         expect(flow).toBeInstanceOf(Flow);
         const transaction: string = fs.readFileSync('cadence/T_SETUP.cdc').toString('utf-8')
             // service account
-            .replace(/[$](service)/g, svc.address.toString('hex'));
-        const txRes = await flow.execute_transaction(transaction, [], [usr1], usr1);
+            .replace(/[$](service)/g, svc.address);
+        const txRes = await flow.execute_transaction(transaction, [], [usr1]);
         if (txRes instanceof Error) return Promise.reject(txRes);
         if (txRes.status_code != 0) return Promise.reject(Error(txRes.error_message));
-        const txRes2 = await flow.execute_transaction(transaction, [], [usr2], usr2);
+        const txRes2 = await flow.execute_transaction(transaction, [], [usr2]);
         if (txRes2 instanceof Error) return Promise.reject(txRes2);
         if (txRes2.status_code != 0) return Promise.reject(Error(txRes2.error_message));
     });
@@ -113,9 +118,9 @@ describe('ContractTesting', () => {
         expect(flow).toBeInstanceOf(Flow);
         const transaction: string = fs.readFileSync('cadence/T_TRANSFER_NFT.cdc').toString('utf-8')
             // service account
-            .replace(/[$](service)/g, svc.address.toString('hex'))
+            .replace(/[$](service)/g, svc.address)
             // buyer account
-            .replace(/[$](buyer)/g, usr1.address.toString('hex'));
+            .replace(/[$](buyer)/g, usr1.address);
         // transfer NFT id 1 to usr1
         const txRes = await flow.execute_transaction(transaction, [1]);
         if (txRes instanceof Error) return Promise.reject(txRes);
@@ -127,9 +132,9 @@ describe('ContractTesting', () => {
         expect(flow).toBeInstanceOf(Flow);
         const script: string = fs.readFileSync('cadence/S_GET_IDS.cdc').toString('utf-8')
             // service account
-            .replace(/[$](service)/g, svc.address.toString('hex'))
+            .replace(/[$](service)/g, svc.address)
             // account to check
-            .replace(/[$](account)/g, usr1.address.toString('hex'));
+            .replace(/[$](account)/g, usr1.address);
         const scRes = await flow.execute_script(script, []);
         if (scRes instanceof Error) return Promise.reject(scRes);
         expect(scRes.value[0].value).toBe('1');
@@ -140,11 +145,12 @@ describe('ContractTesting', () => {
         expect(flow).toBeInstanceOf(Flow);
         const script: string = fs.readFileSync('cadence/S_GET_NFTS.cdc').toString('utf-8')
             // service account
-            .replace(/[$](service)/g, svc.address.toString('hex'))
+            .replace(/[$](service)/g, svc.address)
             // account to check
-            .replace(/[$](account)/g, usr1.address.toString('hex'));
+            .replace(/[$](account)/g, usr1.address);
         const scRes = await flow.execute_script(script, [[1]]);
         if (scRes instanceof Error) return Promise.reject(scRes);
-        expect(scRes.value[0].value).toBe('1f8b080000000000001355ccb10e82301804e05721ffcc40a9a0b0155b22d0686d4be32a03018d98009226c677b7b8b95dbebbdc1b0a0a29f281ca93f8052275a1b48214cc75ec9fafc923e3dc4ff3043e1876ac996b324ef695974ba61d32a7fa0fbd8c70ee1a4af4ba4631de25218a83c05921f2f5fbfca8959d6bdc85dba56d06c578578556df1b2c164b938b31e5ad9543191df27213c1e70bb03a396ca8000000');
+        const decodedNFT = await new Promise<any>((p) => gunzip(Buffer.from(scRes.value[0].value, 'hex'), (e, b) => p(JSON.parse(b.toString('utf-8')))));
+        expect(decodedNFT.ID).toBe(1);
     });
 });
